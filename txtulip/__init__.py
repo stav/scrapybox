10d3207440e18584685f7c6127da98b342caccdb
Steven Almeroth <sroth77@gmail.com>
2016-03-21 18:21:46 -0700

    add txtulip credit

---
+++ txtulip/__init__.py
@@ -0,0 +1,9 @@
+"""
+txtulip: Twisted reactor based on Tulip/Asyncio/Trollius
+========================================================
+
+``txtulip`` is licensed under the MIT open source license, and maintained by
+Itamar Turner-Trauring.
+
+``txtulip`` can be downloaded at https://pypi.python.org/pypi/txtulip
+"""
